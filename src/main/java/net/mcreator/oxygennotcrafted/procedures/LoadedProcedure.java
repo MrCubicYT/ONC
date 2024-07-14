654c226ab233a601e79a98af7dff6d07fa31d5aa
MrCubicYT <Elliot@DESKTOP-NFMDO27>
2024-07-14 17:41:39 +0100

    hmu bbg

---
+++ src/main/java/net/mcreator/oxygennotcrafted/procedures/LoadedProcedure.java
@@ -0,0 +1,36 @@
+package net.mcreator.oxygennotcrafted.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.living.LivingEvent;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
+
+import net.mcreator.oxygennotcrafted.init.OxygenNotCraftedModMobEffects;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class LoadedProcedure {
+	@SubscribeEvent
+	public static void onEntityTick(LivingEvent.LivingTickEvent event) {
+		execute(event, event.getEntity());
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		if (!(entity instanceof LivingEntity _livEnt0 && _livEnt0.hasEffect(OxygenNotCraftedModMobEffects.OXYGENATED.get()))) {
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.WITHER, 60, 1, false, false));
+		}
+	}
+}
